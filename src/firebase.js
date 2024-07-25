ee9f66dc98756c1862df61899a3e5a55759d3004
Iain Liew <liewiain@gmail.com>
2024-07-25 17:00:15 +0800

    .env api protection

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDMnl4wP9Us_4IUsrAoQKkpNoqxQ4DZHPQ",
-    authDomain: "booking-app-7787f.firebaseapp.com",
-    projectId: "booking-app-7787f",
-    storageBucket: "booking-app-7787f.appspot.com",
-    messagingSenderId: "813393444703",
-    appId: "1:813393444703:web:e0aef681f08044611b40ea"
-};
+    apiKey: import.meta.env.apiKey,
+    authDomain: import.meta.env.authDomain,
+    projectId: import.meta.env.projectId,
+    storageBucket: import.meta.env.storageBucket,
+    messagingSenderId: import.meta.env.messagingSenderId,
+    appId: import.meta.env.appId,
+}
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
